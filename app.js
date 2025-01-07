33eb566e0a299ae1e0667c8a913469c97d8d3375
sahithya095 <2211CS010148@mallareddyuniversity.ac.in>
2025-01-07 11:48:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+//global scope
+var a=1;
+a=50;
+document.write(a+a)
+console.log(a+a);
+let b=20;
+b=12;
+console.log(b);
+const c=60;
+console.log(c);
+let _a="sad"
+console.log(_a);
+
+let a="sai"
+{
+    let a="kumar"
+    console.log(a)
+  
+}
+console.log(a)
---
+++ app1.js
@@ -0,0 +1,22 @@
+// let a=1
+// console.log("hi +a+a")
+// document.write('hi +a+a')
+// document.write(`hi +${a+a}`)
+// console.log(`hi +${a+a}`)
+
+// var a='sai'
+// var b='kumar'
+// var age=30
+// let c=`I am ${a+b}, my age is ${age} years old `
+// let d=`React js student = ${a+b} `
+// document.write(d,"<br>")
+
+// console.log(d)
+// document.write(c)
+
+
+// console.log(35>=35)
+// console.log(35>=30)
+// console.log(36>=39)
+
+
